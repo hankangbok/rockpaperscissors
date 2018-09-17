3420bb5ae173dfd6d38e136cd94a371b8043995e
Hannah Kang <kyouyatamax@gmail.com>
2018-09-17 01:34:53 -0700

    sucessfully made  a version that plays five rounds!

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,6 +1,29 @@
+//General global variable to keep track of which round it is
+//Also a variable to keep track of wins/losses
+round=1;
+playerWins=0;
+CPUWins=0;
+ties=0;
+
 function randomNum(){
     let CPUselection=Math.floor(Math.random()*3+1);
     return CPUselection;
+}
+
+function fiverounds(){
+    while (round<=5){
+        game2();
+    }
+    results=playerWins-CPUWins;
+    if (results>0){
+        console.log("Player has won! You won " + playerWins + " times.");
+    }
+    if (results<0){
+        console.log("Player has lost! CPU won " + CPUWins + " times.");
+    }
+    if (results==0){
+        console.log("There's no winner here.");
+    }
 }
 
 function game2(){
@@ -27,20 +50,23 @@ function game2(){
 			break;
 	}
 	if (playernum<4){
-		gamePlay=playernum-CPUselection;
+        round+=1;
+        gamePlay=playernum-CPUselection;
 		switch(gamePlay){
             case 0:
 				console.log("It's a tie. " +" P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1]);
-				break;
+                ties+=1;
+                break;
             case(-1):
 			case(2):
 				console.log("Player 1 wins "  +"P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1]);
-				break;
+                playerWins+=1;
+                break;
 			case(1):
 			case(-2):
 				console.log("CPU wins " +"P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1]);
-				break;
-
+                CPUWins+=1;
+                break;
         }	
      }
 }
