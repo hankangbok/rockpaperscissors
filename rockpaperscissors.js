587d5d8255f4a9ee434caaff711280d9fa696d91
Hannah Kang <kyouyatamax@gmail.com>
2018-09-24 22:54:01 -0700

    button UI version of rock paper scissors, also displays game outcome on the webpage through DOM methods instead of js console log

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -10,22 +10,6 @@ function randomNum(){
     return CPUselection;
 }
 
-function fiverounds(){
-    while (round<=5){
-        game2();
-    }
-    results=playerWins-CPUWins;
-    if (results>0){
-        console.log("Player has won! You won " + playerWins + " times.");
-    }
-    if (results<0){
-        console.log("Player has lost! CPU won " + CPUWins + " times.");
-    }
-    if (results==0){
-        console.log("There's no winner here.");
-    }
-}
-
 function game2(){
 	CPUselection=randomNum();
 	//make the player selection into number.
@@ -54,19 +38,64 @@ function game2(){
         gamePlay=playernum-CPUselection;
 		switch(gamePlay){
             case 0:
+				gameResult = "It's a tie. " +" P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1];	
 				console.log("It's a tie. " +" P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1]);
-                ties+=1;
+				displayWinner(gameResult);
+				ties+=1;
                 break;
             case(-1):
 			case(2):
+				gameResult="Player 1 wins "  +"P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1];
+				displayWinner(gameResult);
 				console.log("Player 1 wins "  +"P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1]);
-                playerWins+=1;
+				playerWins+=1;
                 break;
 			case(1):
 			case(-2):
-				console.log("CPU wins " +"P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1]);
+			    gameResult = "CPU wins " + "P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1];
+				displayWinner(gameResult);
+			    console.log("CPU wins " +"P1 used: "+options[playernum-1] +" CPU used: "+options[CPUselection-1]);
                 CPUWins+=1;
                 break;
         }	
      }
 }
+
+const allbuttons = document.querySelectorAll('button');
+allbuttons.forEach((eachbutton) => {
+    eachbutton.addEventListener('click', game2);
+});
+
+function displayWinner(whoWon) {
+  const container = document.querySelector('#displayresults')
+  const content = document.createElement('div');
+  content.classList.add('content');
+  content.textContent = whoWon;
+  console.log("this should output who won")
+  container.appendChild(content);
+}
+// var rockbutton = document.querySelector('#rock');
+// rockbutton.addEventListener('click', game2);
+
+// var paperbutton = document.querySelector('#paper');
+// paperbutton.addEventListener('click', game2);
+
+// var scissorbutton = document.querySelector('#scissors');
+// scissorbutton.addEventListener('click', game2);
+
+
+//function fiverounds(){
+//     while (round<=5){
+//         game2();
+//     }
+//     results=playerWins-CPUWins;
+//     if (results>0){
+//         console.log("Player has won! You won " + playerWins + " times.");
+//     }
+//     if (results<0){
+//         console.log("Player has lost! CPU won " + CPUWins + " times.");
+//     }
+//     if (results==0){
+//         console.log("There's no winner here.");
+//     }
+// }
